6b5494f3a5e3bf60da4f147607efc1fb4a610e8c
christma <liuchuanmumumu@gmail.com>
2020-02-14 20:57:56 +0800

    run SortTestHelper

--- src/SelectionSort/selectionSort.java
+++ src/SelectionSort/selectionSort.java
@@ -1,14 +1,14 @@
 package SelectionSort;
-
+import java.util.*;
 public class selectionSort {
 
 
-    public static void sort(int[] arr) {
+    public static void sort(Comparable[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             int minIndex = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j].compareTo( arr[minIndex] ) < 0 ) {
                     minIndex = j;
                 }
             }
@@ -20,21 +20,17 @@ public class selectionSort {
 
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int t = arr[i];
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
     }
 
     public static void main(String[] args) {
 
-        int[] arr = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        Integer[] arr = SortTestHelper.generateRandomArray(100,0,100);
         selectionSort.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            System.out.print(' ');
-        }
-        System.out.println();
+        SortTestHelper.printArray(arr);
     }
 
 }
