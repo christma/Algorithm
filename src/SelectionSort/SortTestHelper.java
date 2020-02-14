1f2d18186a3013f3d260783e851b62741a3fcb2d
christma <liuchuanmumumu@gmail.com>
2020-02-14 21:18:34 +0800

    add testSort Method

--- src/SelectionSort/SortTestHelper.java
+++ src/SelectionSort/SortTestHelper.java
@@ -1,5 +1,8 @@
 package SelectionSort;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.lang.reflect.Method;
+
 public class SortTestHelper {
     private SortTestHelper() {
     }
@@ -21,4 +24,35 @@ public class SortTestHelper {
         }
         System.out.println();
     }
+
+
+    public static void testSort(String sortClassName, Comparable[] arr) {
+
+        try {
+            Class sortClass = Class.forName(sortClassName);
+            Method sortMethod = sortClass.getMethod("sort", new Class[]{Comparable[].class});
+            Object[] params = new Object[]{arr};
+            long startTime = System.currentTimeMillis();
+            sortMethod.invoke(null, params);
+            long endTime = System.currentTimeMillis();
+            assert isSorted(arr);
+
+            System.out.println(sortClass.getSimpleName() + " : " + (endTime - startTime) + " ms");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static boolean isSorted(Comparable[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].compareTo(arr[i + 1]) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
