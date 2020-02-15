08771b5b3cd6104e5accf9296e528e72c51cb233
christma <liuchuanmumumu@gmail.com>
2020-02-15 16:38:29 +0800

    Add InsertionSort Method

---
+++ src/InsertionSort/InsertionSort.java
@@ -0,0 +1,45 @@
+package InsertionSort;
+
+import SelectionSort.SortTestHelper;
+
+public class InsertionSort {
+    private InsertionSort() {
+
+    }
+
+    public static void sort(Comparable[] arr) {
+
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            // method 1
+//            for (int j = i; j > 0; j--) {
+//                if (arr[j].compareTo(arr[j - 1]) < 0) {
+//                    swap(arr, j, j - 1);
+//                } else {
+//                    break;
+//                }
+//            }
+            // method 2
+
+            for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
+                swap(arr, j, j - 1);
+            }
+
+        }
+    }
+
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+
+    public static void main(String[] args) {
+
+        int N = 100000;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+        SortTestHelper.testSort("InsertionSort.InsertionSort", arr);
+
+    }
+}
