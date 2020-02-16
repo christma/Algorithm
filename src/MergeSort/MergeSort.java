8b480d5ac9b3dd0599f703716ecec62f7b68f61a
christma <liuchuanmumumu@gmail.com>
2020-02-16 21:15:06 +0800

    Add MergeSort Method

--- src/MergeSort/MergeSort.java
+++ src/MergeSort/MergeSort.java
@@ -24,7 +24,10 @@ public class MergeSort {
         int mid = (l + r) / 2;
         sort(arr, l, mid);
         sort(arr, mid + 1, r);
-        merge(arr, l, mid, r);
+        if (arr[mid].compareTo(arr[mid + 1]) > 0) {
+            merge(arr, l, mid, r);
+        }
+
     }
 
     private static void merge(Comparable[] arr, int l, int mid, int r) {
