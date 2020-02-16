e179ce5b1d0fbb3c2585957457ab9c0b4688212f
christma <liuchuanmumumu@gmail.com>
2020-02-16 09:52:09 +0800

    Add MergeSort Method

---
+++ src/MergeSort/MergeSort.java
@@ -0,0 +1,62 @@
+package MergeSort;
+
+import SelectionSort.SortTestHelper;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    private MergeSort() {
+    }
+
+
+    public static void sort(Comparable[] arr) {
+        int n = arr.length;
+        sort(arr, 0, n - 1);
+    }
+
+    private static void sort(Comparable[] arr, int l, int r) {
+
+        if (l >= r) {
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        sort(arr, l, mid);
+        sort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private static void merge(Comparable[] arr, int l, int mid, int r) {
+
+        Comparable[] aux = Arrays.copyOfRange(arr, l, r + 1);
+
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {
+                arr[k] = aux[j - l];
+                j++;
+            } else if (j > r) {
+                arr[k] = aux[i - l];
+                i++;
+            } else if (aux[i - l].compareTo(aux[j - l]) < 0) {
+                arr[k] = aux[i - l];
+                i++;
+            } else {
+                arr[k] = aux[j - l];
+                j++;
+            }
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        int N = 1000000;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+        SortTestHelper.testSort("MergeSort.MergeSort", arr);
+    }
+
+}
