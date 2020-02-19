59b04613653acb8fe59135d2eec673072a13d54d
christma <liuchuanmumumu@gmail.com>
2020-02-19 21:12:42 +0800

    Add Shift Down Method

--- src/MaxHeap/MaxHeap.java
+++ src/MaxHeap/MaxHeap.java
@@ -1,24 +1,52 @@
 package MaxHeap;
 
-public class MaxHeap<Item> {
-    private Item[] data;
-    private int count;
+public class MaxHeap<Item extends Comparable> {
 
-    public MaxHeap(int capacity){
-        data = (Item[]) new Object[capacity+1];
+    protected Item[] data;
+    protected int count;
+    protected int capacity;
+
+    public MaxHeap(int capacity) {
+        data = (Item[]) new Comparable[capacity + 1];
         count = 0;
+        this.capacity = capacity;
     }
 
-    public int size(){
+    public int size() {
         return count;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return count == 0;
     }
 
+    public void insert(Item item) {
+        assert count + 1 <= capacity;
+        data[count + 1] = item;
+        count++;
+        shiftUp(count);
+    }
+
+    private void shiftUp(int k) {
+        while (k > 1 && data[k / 2].compareTo(data[k]) < 0) {
+            swap(k, k / 2);
+            k /= 2;
+        }
+    }
+
+    private void swap(int i, int j) {
+        Item t = data[i];
+        data[i] = data[j];
+        data[j] = t;
+    }
+
+
     public static void main(String[] args) {
         MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(100);
+        int N = 50; // 堆中元素个数
+        int M = 100; // 堆中元素取值范围[0, M)
+        for( int i = 0 ; i < N ; i ++ )
+            maxHeap.insert( new Integer((int)(Math.random() * M)) );
         System.out.println(maxHeap.size());
     }
 
