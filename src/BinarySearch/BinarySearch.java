294663e65596c223aff68abc76484a678a8d21ef
christma <liuchuanmumumu@gmail.com>
2020-02-23 05:47:42 +0800

    Add Main Method

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -46,23 +46,14 @@ public class BinarySearch {
 
     }
 
+    public static int find1(Comparable[] arr, Comparable target) {
+
+        return find2(arr, 0, arr.length - 1, target);
+    }
+
 
     public static void main(String[] args) {
-        int N = 1000000;
-        Integer[] arr = new Integer[N];
-        for (int i = 0; i < N; i++)
-            arr[i] = new Integer(i);
 
-        // 对于我们的待查找数组[0...N)
-        // 对[0...N)区间的数值使用二分查找，最终结果应该就是数字本身
-        // 对[N...2*N)区间的数值使用二分查找，因为这些数字不在arr中，结果为-1
-        for (int i = 0; i < 2 * N; i++) {
-            int v = BinarySearch.find(arr, new Integer(i));
-            if (i < N)
-                assert v == i;
-            else
-                assert v == -1;
-        }
 
     }
 }
