21ad7520f29836e01bea6f399f6385f6a73e48bb
christma <liuchuanmumumu@gmail.com>
2020-02-29 19:54:19 +0800

    Add UnionFind2 Method

---
+++ src/UnoinFind/UnionFind2.java
@@ -0,0 +1,41 @@
+package UnoinFind;
+
+public class UnionFind2 {
+
+    private int[] parent;
+    private int count;
+
+    public UnionFind2(int count) {
+        parent = new int[count];
+        this.count = count;
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+        }
+    }
+
+    private int find(int p) {
+        assert (p > 0 && p < count);
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+        parent[pRoot] = qRoot;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
