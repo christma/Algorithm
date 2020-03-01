308d2e279d05bf8584a6f9e4b0011ef4c12a8cb0
christma <liuchuanmumumu@gmail.com>
2020-03-01 10:02:43 +0800

    Add UnionFind3 Method

---
+++ src/UnoinFind/UnionFind3.java
@@ -0,0 +1,53 @@
+package UnoinFind;
+
+public class UnionFind3 {
+    private int[] parent;
+    private int[] sz;
+    private int count;
+
+
+    public UnionFind3(int count) {
+        parent = new int[count];
+        sz = new int[count];
+        this.count = count;
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+
+    private int find(int p) {
+        assert (p >= 0 && p < count);
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+        return p;
+    }
+
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        if (sz[pRoot] < sz[qRoot]) {
+            parent[pRoot] = qRoot;
+            sz[qRoot] += sz[pRoot];
+        } else {
+            parent[qRoot] = pRoot;
+            sz[pRoot] += sz[qRoot];
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
