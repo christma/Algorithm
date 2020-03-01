cb06eefd51459fdfd3be845336034bd26a2e232f
christma <liuchuanmumumu@gmail.com>
2020-03-01 10:33:20 +0800

    optimization UnionFind5 Method of Path Compression

--- src/UnoinFind/UnionFindTestHelper.java
+++ src/UnoinFind/UnionFindTestHelper.java
@@ -24,6 +24,7 @@ public class UnionFindTestHelper {
         // 打印输出对这2n个操作的耗时
         System.out.println("UF1, " + 2 * n + " ops, " + (endTime - startTime) + "ms");
     }
+
     public static void testUF2(int n) {
 
         UnionFind2 uf = new UnionFind2(n);
@@ -47,6 +48,7 @@ public class UnionFindTestHelper {
         // 打印输出对这2n个操作的耗时
         System.out.println("UF2, " + 2 * n + " ops, " + (endTime - startTime) + "ms");
     }
+
     public static void testUF3(int n) {
 
         UnionFind3 uf = new UnionFind3(n);
@@ -69,5 +71,29 @@ public class UnionFindTestHelper {
 
         // 打印输出对这2n个操作的耗时
         System.out.println("UF3, " + 2 * n + " ops, " + (endTime - startTime) + "ms");
+    }
+
+    public static void testUF5(int n) {
+
+        UnionFind5 uf = new UnionFind5(n);
+
+        long startTime = System.currentTimeMillis();
+
+        // 进行n次操作, 每次随机选择两个元素进行合并操作
+        for (int i = 0; i < n; i++) {
+            int a = (int) (Math.random() * n);
+            int b = (int) (Math.random() * n);
+            uf.unionElements(a, b);
+        }
+        // 再进行n次操作, 每次随机选择两个元素, 查询他们是否同属一个集合
+        for (int i = 0; i < n; i++) {
+            int a = (int) (Math.random() * n);
+            int b = (int) (Math.random() * n);
+            uf.isConnected(a, b);
+        }
+        long endTime = System.currentTimeMillis();
+
+        // 打印输出对这2n个操作的耗时
+        System.out.println("UF5, " + 2 * n + " ops, " + (endTime - startTime) + "ms");
     }
 }
