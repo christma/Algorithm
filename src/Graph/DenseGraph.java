757f0d3502400217a58fe01c39d48726aa0f0030
christma <liuchuanmumumu@gmail.com>
2020-03-02 21:53:40 +0800

    Add DenseGraph Method

---
+++ src/Graph/DenseGraph.java
@@ -0,0 +1,50 @@
+package Graph;
+
+public class DenseGraph {
+
+    private int n; //节点数
+    private int m; // 边数
+    private boolean directed;
+    private boolean[][] g;
+
+    public DenseGraph(int n, boolean directed) {
+        assert n >= 0;
+        this.n = n;
+        this.m = 0;
+        this.directed = directed;
+        g = new boolean[n][n];
+    }
+
+    public int V() {
+        return n;
+    }
+
+    public int E() {
+        return m;
+    }
+
+    // 向图中添加一个边
+    public void addEdge(int v, int w) {
+        assert v >= 0 && v < n;
+        assert w >= 0 && w < n;
+
+        if (hasEdge(v, m)) {
+            return;
+        }
+
+        g[v][w] = true;
+        if (!directed) {
+            g[w][v] = true;
+        }
+        m++;
+    }
+
+    private boolean hasEdge(int v, int w) {
+        assert v >= 0 && v < n;
+        assert w >= 0 && w < n;
+
+        return g[v][w];
+    }
+
+
+}
