09c75e59b238c7e96e17bb4e7fd40d0a1b1d7887
christma <liuchuanmumumu@gmail.com>
2020-03-07 22:04:00 +0800

    Weighted Graph Method

---
+++ src/MinimumSpanTrees/Edge.java
@@ -0,0 +1,50 @@
+package MinimumSpanTrees;
+
+public class Edge<Weight extends Number & Comparable> implements Comparable<Edge> {
+    private int a, b; // 边的两个端点
+    private Weight weight; // 边的权值
+
+    public Edge(int a, int b, Weight weight) {
+        this.a = a;
+        this.b = b;
+        this.weight = weight;
+    }
+
+    public Edge(Edge<Weight> e) {
+        this.a = e.a;
+        this.b = e.b;
+        this.weight = e.weight;
+    }
+
+    private int v() {
+        return a;
+    }
+
+    private int w() {
+        return b;
+    }
+
+    public Weight wt() {
+        return weight;
+    }
+
+    public int other(int x) {
+        assert x == a || x == b;
+        return x == a ? b : a;
+    }
+
+    public String toString() {
+        return "" + a + "-" + b + ": " + weight;
+    }
+
+    // 边之间的比较
+    public int compareTo(Edge that) {
+        if (weight.compareTo(that.wt()) < 0) {
+            return -1;
+        } else if (weight.compareTo(this.wt()) > 0) {
+            return +1;
+        } else {
+            return 0;
+        }
+    }
+}
