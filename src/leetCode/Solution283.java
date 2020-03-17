d15761512d3b4505277ca08240abed2e77a895ce
christma <liuchuanmumumu@gmail.com>
2020-03-17 20:52:08 +0800

    leetCode 283 move zero

--- src/leetCode/Solution283.java
+++ src/leetCode/Solution283.java
@@ -22,7 +22,33 @@ public class Solution283 {
 
     }
 
+    private static void printArr(int[] arr){
+        for(int i = 0 ; i < arr.length ; i ++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public void moveZeroes1(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[k++] = nums[i];
+            }
+        }
+        for (int i = k; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+
     public static void main(String[] args) {
 
+        Solution283 solution283 = new Solution283();
+        int[] arr = {0, 1, 0, 2, 19};
+//        solution283.moveZeroes(arr);
+//        printArr(arr);
+
+        solution283.moveZeroes1(arr);
+        printArr(arr);
     }
 }
