ea7568f17273c3e1e7b5fd291aedde434b3316fd
christma <liuchuanmumumu@gmail.com>
2020-06-24 23:04:22 +0800

    Solution144

--- src/leetCode/Solution144.java
+++ src/leetCode/Solution144.java
@@ -1,7 +1,9 @@
 package leetCode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution144 {
     public class TreeNode {
@@ -15,6 +17,7 @@ public class Solution144 {
     }
 
     List<Integer> list = new ArrayList<>();
+
     public List<Integer> preorderTraversal(TreeNode root) {
 
         if (root != null) {
@@ -24,5 +27,28 @@ public class Solution144 {
         }
         return list;
     }
+
+
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+        return output;
+    }
+
 
 }
