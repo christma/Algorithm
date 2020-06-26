71793e39921b1764da65e4aa2e21ee50962d913d
christma <liuchuanmumumu@gmail.com>
2020-06-26 09:24:04 +0800

    Solution103 add main

--- src/leetCode/Solution103.java
+++ src/leetCode/Solution103.java
@@ -43,7 +43,7 @@ public class Solution103 {
                 }
             } else {
                 ans.add(temp);
-                temp = new LinkedList<>();
+                temp.clear();
                 if (!queue.isEmpty()) {
                     queue.addLast(null);
                 }
