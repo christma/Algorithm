54b6737637264e0b389275106e302edf5d211fc8
christma <liuchuanmumumu@gmail.com>
2020-07-05 14:15:55 +0800

    Solution 101

---
+++ src/leetCode/Solution101.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+public class Solution101 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.right, q.left) && check(p.left, q.right);
+    }
+}
