ba62040e9d8e7c3fbf07dd61ff1ce9345908a714
Christmas <46958618+christma@users.noreply.github.com>
2020-08-18 10:14:28 +0800

    Solution109

---
+++ src/leetCode/Solution109.java
@@ -0,0 +1,75 @@
+package com.spring.leetCode;
+
+public class Solution109 {
+    ListNode globalHead;
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        globalHead = head;
+        int length = getLength(head);
+        return buildTree(0, length - 1);
+    }
+
+    private TreeNode buildTree(int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = (left + right + 1) / 2;
+        TreeNode root = new TreeNode();
+        root.left = buildTree(left, mid - 1);
+        root.val = globalHead.val;
+        globalHead = globalHead.next;
+        root.right = buildTree(mid + 1, right);
+        return root;
+    }
+
+    private int getLength(ListNode node) {
+        int l = 0;
+        while (node != null) {
+            ++l;
+            node = node.next;
+        }
+        return l;
+    }
+
+
+
+
+}
